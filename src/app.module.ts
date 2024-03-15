7b9fdd87f73befe5967a13b165cace7a795ea72a
Ungs <iqeq1945@gmail.com>
2024-03-15 17:07:41 +0900

    first commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,8 @@ import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaService } from './prisma/prisma.service';
 import { UserService } from './user/user.service';
+import { UserController } from './user/user.controller';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
@@ -15,8 +17,9 @@ import { UserService } from './user/user.service';
       envFilePath: `.${process.env.NODE_ENV}.env`,
     }),
     AuthModule,
+    UserModule,
   ],
-  controllers: [AppController, AuthController],
+  controllers: [AppController, AuthController, UserController],
   providers: [AppService, AuthService, PrismaService, UserService],
 })
 export class AppModule {}
---
+++ src/user/user.controller.ts
@@ -0,0 +1,19 @@
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { UserService } from './user.service';
+import { User as UserModel } from '@prisma/client';
+
+@Controller('user')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Get(':id')
+  async getUserInfo(@Param('id') id: string): Promise<UserModel> {
+    return this.userService.user({ id });
+  }
+  @Post('signup')
+  async signupUser(
+    @Body() userData: { email: string; name: string },
+  ): Promise<UserModel> {
+    return this.userService.createUser(userData);
+  }
+}
